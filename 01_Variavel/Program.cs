41d257994ac7e3c23d0eb2c43cf066f9b596bc02
rafael chaves <rafaelaugustochaves@hotmail.com>
2022-02-14 15:16:53 -0300

    Aula de Variaveis/ byte sbyte e atribuição de seus valores

---
+++ 01_Variavel/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace _01_Variavel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte meuByte;
+            System.Byte meuByte2;
+
+            meuByte = 1;
+            meuByte2 = meuByte;
+            sbyte meuSByte = -10;
+
+            Console.WriteLine("Digite um numero");
+
+            string meuByteLido = Console.ReadLine();
+
+            Console.WriteLine(meuByte2 + "  " + meuByteLido + "  " +meuSByte);
+            
+        }
+    }
+}
